87af72d478c82077feb9912f9d5bdba5375627f0
kihno <stistyle13@gmail.com>
2022-02-08 12:59:44 -0800

    grid loads and clear button creates new user defined grid

---
+++ javascript.js
@@ -0,0 +1,48 @@
+// Variables
+const gridContainer = document.querySelector('#grid');
+const buttonOne = document.querySelector('.button1');
+const buttonTwo = document.querySelector('.button2');
+const clear = document.querySelector('.clear');
+
+
+/* buttonOne.addEventListener('click', () => {
+    createGrid(32);
+});
+
+buttonTwo.addEventListener('click', () => {
+    createGrid(64);
+});
+*/
+
+function removeCells() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+clear.addEventListener('click', () => {
+    removeCells();
+    let num = prompt('How many rows would you like?');
+    if (num < 0 || num > 100 ) {
+        num = prompt('Please specify a number between 0 and 100');
+    }
+    createGrid(num);
+})
+
+function createGrid(x) {
+
+    for (let i = 0; i < x * x; i++) {
+        const createCell = document.createElement('div');
+        createCell.className = 'cell';
+        gridContainer.appendChild(createCell);
+    }
+    gridContainer.style.grid = `repeat(${x}, 1fr) / repeat(${x}, 1fr)`;
+};
+
+document.onload = createGrid(16);
+
+const cell = document.querySelector('.cell');
+cell.addEventListener('mouseover', () => {
+    const draw = document.getElementsByClassName('.cell')
+    draw.classList.add('draw');
+});
